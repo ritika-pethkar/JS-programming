52a4199a80c76821f803a87420aff784421a96a8
ritika-pethkar <139866068+ritika-pethkar@users.noreply.github.com>
2024-07-20 18:33:30 +0000

    understanding comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+/*
+console.log(2<1);
+console.log(2<=1);
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+*/
+console.log("2" > 1);
+console.log("02" > 1);
+
+/*
+console.log(null > 0);  false
+console.log(null >= 0); true => null is converted into 0
+console.log(null == 0); false
+Confusing comparisons
+*/
+
+// === -> strictly check values along with datatype
+console.log("2" === 2); // false
