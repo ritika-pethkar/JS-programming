bc9b8ba1471102eda910826279cf7f214240283c
ritika-pethkar <139866068+ritika-pethkar@users.noreply.github.com>
2024-07-21 07:16:48 +0000

    memory types in javascript

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -39,4 +39,32 @@ const myFun = function(){
     console.log("hello world")
 }
 
-console.log(typeof myFun)
+// console.log(typeof myFun)
+
+// ---------------------- Memory Type in Javascript----------------------
+
+//Stack(Primitive) => copy of object , Heap(Non-Primitive) => Reference of object
+
+/*
+anotherName contains copy of myName not the original one,
+that is why when we change anotherName myName will not be affected
+*/
+
+let myName = "Ritika"
+let anotherName = myName
+anotherName = "Somya"
+
+console.log(anotherName);
+console.log(myName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl.com"
+}
+
+let userTwo = userOne // this will point to the same original userOne object
+userTwo.email = "ritika@google.com" // it will also change userOne's email 
+
+console.log("userTwo -", userTwo.email);
+console.log("userOne -",userOne.email);
+
