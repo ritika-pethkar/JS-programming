1105511e3c7718ea014bb55958220376b563ee14
ritika-pethkar <139866068+ritika-pethkar@users.noreply.github.com>
2024-07-25 07:48:37 +0000

    while and do while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,20 @@
+// let i = 1;
+// while (i <= 10) {
+//     //console.log(`i is : ${i}`);
+//     i++
+// }
+
+/*
+let myArray = ['H', 'E', 'L', 'L', 'O']
+let index = 0;
+while (index < myArray.length) {
+    console.log(`Value is ${myArray[index]}`);
+    index++
+}
+*/
+
+let score = 11;
+do {
+   console.log(`Score is ${score}`); 
+   score++
+} while (score <= 10);
