08aaf23b7592d6a431470750c9b22eb41d19f707
ritika-pethkar <139866068+ritika-pethkar@users.noreply.github.com>
2024-07-27 09:52:44 +0000

    project03 - digital clock

---
+++ 07_projects/project03/one.js
@@ -0,0 +1,8 @@
+// const time = document.getElementById('clock')
+const time = document.querySelector('#clock');
+
+setInterval(function () {
+  let date = new Date();
+  // console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
